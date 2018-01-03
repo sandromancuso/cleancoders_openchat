013fea0320403e6b444180bee03dffc59b26cbcf
Sandro Mancuso <sandromancuso@yahoo.com>
2018-01-03 19:07:12 +0000

    Cosmetic change - static import

--- src/main/java/org/openchat/web/api/UserAPI.java
+++ src/main/java/org/openchat/web/api/UserAPI.java
@@ -2,13 +2,13 @@ package org.openchat.web.api;
 
 import org.openchat.core.actions.RetrieveAllUsers;
 import org.openchat.core.domain.user.User;
-import org.openchat.web.infrastructure.jsonparsers.UserToJson;
 import spark.Request;
 import spark.Response;
 
 import java.util.List;
 
 import static org.eclipse.jetty.http.HttpStatus.OK_200;
+import static org.openchat.web.infrastructure.jsonparsers.UserToJson.jsonFor;
 
 public class UserAPI {
     private static final String JSON = "application/json";
@@ -22,7 +22,7 @@ public class UserAPI {
         List<User> users = retrieveAllUsers.execute();
         response.status(OK_200);
         response.type(JSON);
-        return UserToJson.jsonFor(users);
+        return jsonFor(users);
     }
 
 }
