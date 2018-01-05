91a433a6860af22edc2d3c24b0beb911f7fcd46c
Sandro Mancuso <sandromancuso@yahoo.com>
2018-01-05 19:48:22 +0000

    APITestSuite skeleton

---
+++ src/main-test/java/acceptance/APITestSuit.java
@@ -0,0 +1,21 @@
+package acceptance;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+
+})
+public class APITestSuit {
+
+    @BeforeClass
+    public static void setUp() {
+    }
+
+    @AfterClass
+    public static void tearDown() {
+    }
+}
