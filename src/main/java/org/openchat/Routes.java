1ffcd2c339885cde050932a3d71e616c0ead2f73
Sandro Mancuso <sandromancuso@yahoo.com>
2018-01-30 20:22:26 +0100

    Created Swagger routes

--- src/main/java/org/openchat/Routes.java
+++ src/main/java/org/openchat/Routes.java
@@ -1,10 +1,26 @@
 package org.openchat;
 
 import static spark.Spark.get;
+import static spark.Spark.options;
 
 public class Routes {
 
     public void create() {
+        swaggerRoutes();
+        openchatRoutes();
+    }
+
+    private void openchatRoutes() {
         get("status", (req, res) -> "OpenChat: OK!");
+    }
+
+    private void swaggerRoutes() {
+        options("registration", (req, res) -> "OK");
+        options("login", (req, res) -> "OK");
+        options("user/{userId}/posts", (req, res) -> "OK");
+        options("follow", (req, res) -> "OK");
+        options("user/{userId}/wall", (req, res) -> "OK");
+        options("users", (req, res) -> "OK");
+        options("user/{userId}/followees", (req, res) -> "OK");
     }
 }
