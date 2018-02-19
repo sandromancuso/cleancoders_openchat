1f18a0d5b406a1fab93a48148d2d6c47823cb6a3
Sandro Mancuso <sandromancuso@yahoo.com>
2018-02-19 14:58:16 -0600

    First test for PostService

---
+++ src/test/java/org/openchat/domain/posts/PostServiceShould.java
@@ -0,0 +1,12 @@
+package org.openchat.domain.posts;
+
+import org.junit.Test;
+
+public class PostServiceShould {
+    
+    @Test public void
+    () {
+        assertThat().isEqualTo();
+    } 
+
+}
