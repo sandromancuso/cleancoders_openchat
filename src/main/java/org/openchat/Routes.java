6cff92448198f2defc2da450811123de39700440
Robert C. Martin <unclebob@cleancoder.com>
2018-02-20 14:50:27 -0600

    get ready to start

--- src/main/java/org/openchat/Routes.java
+++ src/main/java/org/openchat/Routes.java
@@ -6,7 +6,6 @@ import static spark.Spark.options;
 public class Routes {
 
     public void create() {
-        swaggerRoutes();
         openchatRoutes();
     }
 
@@ -14,12 +13,4 @@ public class Routes {
         get("status", (req, res) -> "OpenChat: OK!");
     }
 
-    private void swaggerRoutes() {
-        options("users", (req, res) -> "OK");
-        options("login", (req, res) -> "OK");
-        options("users/:userId/timeline", (req, res) -> "OK");
-        options("followings", (req, res) -> "OK");
-        options("followings/:userId/followees", (req, res) -> "OK");
-        options("users/:userId/wall", (req, res) -> "OK");
-    }
 }
