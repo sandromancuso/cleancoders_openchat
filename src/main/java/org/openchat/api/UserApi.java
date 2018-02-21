3d7b7fd79092d6c59f70534e815a468184f2a7a7
Robert C. Martin <unclebob@cleancoder.com>
2018-02-21 10:57:31 -0600

    E7T1

--- src/main/java/org/openchat/api/UserApi.java
+++ src/main/java/org/openchat/api/UserApi.java
@@ -1,17 +1,42 @@
 package org.openchat.api;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
+import org.openchat.entities.User;
 import org.openchat.usecases.CreateUser;
 import org.openchat.usecases.CreateUserRequest;
+import org.openchat.usecases.Repository;
 import spark.Request;
 import spark.Response;
 
 public class UserApi {
   public String registerUser(Request req, Response res) {
+    CreateUserRequest createUserRequest = makeCreateUserRequest(req.body());
     CreateUser useCase = new CreateUser();
-    CreateUserRequest useCaseRequest = new CreateUserRequest();
 
-    useCase.createUser(useCaseRequest);
+    try {
+      User user = useCase.createUser(createUserRequest);
+      return makeCreatedUserResponse(user, res);
+    } catch (Repository.DuplicateUser e) {
+      return makeDuplicateUserReponse(res);
+    }
+  }
 
-    return "stuff";
+  protected String makeDuplicateUserReponse(Response res) {
+    res.status(400);
+    return "Username already in use.";
+  }
+
+  protected String makeCreatedUserResponse(User user, Response res) {
+    return null;
+  }
+
+  protected CreateUserRequest makeCreateUserRequest(String jsonString) {
+    CreateUserRequest request = new CreateUserRequest();
+    JsonObject json = Json.parse(jsonString).asObject();
+    request.username = json.getString("username", null);
+    request.password = json.getString("password", null);
+    request.about = json.getString("about", null);
+    return request;
   }
 }
--- src/test/java/org/openchat/api/UserApiTest.java
+++ src/test/java/org/openchat/api/UserApiTest.java
@@ -1,13 +1,39 @@
 package org.openchat.api;
 
+import com.eclipsesource.json.JsonObject;
+import org.junit.Before;
 import org.junit.Test;
-import spark.Request;
+import org.openchat.usecases.CreateUserRequest;
+import spark.Response;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserApiTest {
-  @Test
-  public void canAddUser() throws Exception {
+
+  private UserApi userApi;
+
+  @Before
+  public void setUp() throws Exception {
+    userApi = new UserApi();
   }
 
+  @Test
+  public void canMakeRequest() throws Exception {
+    String json = new JsonObject()
+                              .add("username", "username")
+                              .add("password", "password")
+                              .add("about", "about").toString();
 
+    CreateUserRequest cur = userApi.makeCreateUserRequest(json);
+    assertThat(cur.username).isEqualTo("username");
+    assertThat(cur.password).isEqualTo("password");
+    assertThat(cur.about).isEqualTo("about");
+  }
+
+  @Test
+  public void createsAppropriateResponseForDuplicateUser() throws Exception {
+    Response res = new Response();
+    userApi.makeDuplicateUserReponse(res)
+  }
 
 }
