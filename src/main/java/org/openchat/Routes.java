1454a1e844aee3bedb93b9930b27f6dd35de35d0
Robert C. Martin <unclebob@cleancoder.com>
2018-02-22 06:01:44 -0600

    Bob cheats and turns integration tests into unit tests.

--- src/main/java/org/openchat/Routes.java
+++ src/main/java/org/openchat/Routes.java
@@ -5,11 +5,11 @@ import org.openchat.api.LoginApi;
 import org.openchat.api.UserApi;
 import org.openchat.usecases.UseCaseContext;
 
-import static spark.Spark.get;
-import static spark.Spark.options;
-import static spark.Spark.post;
+import static spark.Spark.*;
+
 
 public class Routes {
+    private static boolean TEST_CONFIG = true; // should be environment variable.
 
     private UserApi userApi;
     private LoginApi loginAPI;
@@ -23,6 +23,8 @@ public class Routes {
     }
 
     private void openchatRoutes() {
+        if (TEST_CONFIG)
+            delete("repositories", (req, res) -> {APIContext.initialize(); UseCaseContext.initialize(); return "";});
         get("status", (req, res) -> "OpenChat: OK!");
         post("users", (req, res) -> userApi.registerUser(req, res));
         post("login", (req, res) -> loginAPI.login(req, res));
--- src/test-integration/java/integration/IT_RegistrationAPI.java
+++ src/test-integration/java/integration/IT_RegistrationAPI.java
@@ -1,29 +1,55 @@
 package integration;
 
 import com.eclipsesource.json.JsonObject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openchat.OpenChat;
+import org.openchat.OpenChatLauncher;
+import org.openchat.api.APIContext;
+import org.openchat.usecases.UseCaseContext;
 
 import static integration.APITestSuit.BASE_URL;
 import static integration.APITestSuit.UUID_PATTERN;
+import static io.restassured.RestAssured.delete;
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.post;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.matchesPattern;
 
 public class IT_RegistrationAPI {
+    @Before
+    public void initialize() {
+        delete(BASE_URL+"/repositories");
+    }
 
     @Test public void
     register_a_new_user() {
         given()
-                .body(withJsonContaining("Lucy", "alki324d", "About Lucy"))
+                .body(withJsonContaining("username", "password", "about"))
         .when()
                 .post(BASE_URL + "/users")
         .then()
                 .statusCode(201)
                 .contentType(JSON)
                 .body("id", matchesPattern(UUID_PATTERN))
-                .body("username", is("Lucy"))
-                .body("about", is("About Lucy"));
+                .body("username", is("username"))
+                .body("about", is("about"));
+    }
+
+    @Test
+    public void register_a_duplicate_user() throws Exception {
+          given()
+                .body(withJsonContaining("username", "password", "about"))
+                .post(BASE_URL+"/users");
+          given()
+                .body(withJsonContaining("username", "xyzzy", "about"))
+          .when()
+                .post(BASE_URL + "/users")
+          .then()
+                .statusCode(400)
+                .assertThat().body(is("Username already in use."));
     }
 
     private String withJsonContaining(String username, String password, String about) {
